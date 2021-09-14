544e1f23b90f9e2435aec30a6a44b1b1bf5d4b9b
Sandy Chuchuca <sandy@settled.co.uk>
2021-09-14 11:42:30 +0100

    Add addToFavourites function

--- pages/gifs/index.tsx
+++ pages/gifs/index.tsx
@@ -12,6 +12,27 @@ export async function getStaticProps() {
   // Filtering as there are too many domain to configure in next.config.js
   const gifs = data.filter((gif: Gif) => gif.images.fixed_height.url.includes('media2.giphy.com'))
 
+  // const a = {
+  //   type: 'gif',
+  //   id: '26TM88tPoYX5QNGESP',
+  //   url: 'https://giphy.com/gifs/reaction-mood-26TM88tPoYX5QNGESP',
+  //   slug: 'reaction-mood-26TM88tPoYX5QNGESP',
+  //   bitly_gif_url: 'http://gph.is/2p2Vi71',
+  //   bitly_url: 'http://gph.is/2p2Vi71',
+  //   embed_url: 'https://giphy.com/embed/26TM88tPoYX5QNGESP',
+  //   username: '',
+  //   source: '',
+  //   title: 'shoes GIF',
+  //   rating: 'g',
+  //   content_url: '',
+  //   source_tld: '',
+  //   source_post_url: '',
+  //   is_sticker: 0,
+  //   import_datetime: '2018-03-09 19:45:35',
+  //   trending_datetime: '0000-00-00 00:00:00',
+  //   images: {}
+  // }
+
   return {
     props: {
       gifs,
@@ -19,13 +40,28 @@ export async function getStaticProps() {
   }
 }
 
+const addToFavourites = (gifId: string) => {
+  console.warn('No implemented!')
+  // make a api call
+}
+
 const Gifs: NextPage = ({ gifs }: any) => {
   return (<>
     <div>Gifs </div>
+
     <ul>
-      <ul>
-        {!!gifs || !!gifs?.length ? gifs.map((gif: any) => <li key={gif.id}><Image src={gif.images.fixed_height.url} alt="Gif" height="200" width="200" /></li>) : ""}
-      </ul>
+      {!!gifs || !!gifs?.length
+        ? gifs.map((gif: any) =>
+          <li key={gif.id}>
+            <div onClick={()=>addToFavourites(gif.id)}>
+              <Image 
+                src={gif.images.fixed_height.url} 
+                alt="Gif" 
+                height="200" 
+                width="200" />
+            </div>
+          </li>)
+        : ""}
     </ul>
   </>)
 }
